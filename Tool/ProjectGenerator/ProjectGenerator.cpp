0e2b45e6dd1b90c68c192002bb12ebdd82737101
ath331 <kstnftk@naver.com>
2023-05-31 23:33:16 +0900

    프로젝트 제너레이터 임시 작업
    코어쪽 작업 얼추하고 컨텐츠 작업할떄 반영하기

--- Tool/ProjectGenerator/ProjectGenerator.cpp
+++ Tool/ProjectGenerator/ProjectGenerator.cpp
@@ -1,21 +1,228 @@
-﻿// ProjectGenerator.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+#include <cstring>
+#include <filesystem>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+
+
+using namespace std;
+namespace fs = filesystem;
+
+
+vector< string > g_cppVec;
+vector< string > g_hVec;
+
+
+filesystem::path g_rootDirectory = "C:\\Users\ath331\\Desktop\\a\\VisualStudio\\C++\\ServerEngine\\Server\\GameServer";
+string g_rootStr( g_rootDirectory.string() );
+int rootSize = g_rootStr.size();
+
+
+void PrintDirectory( int depth, const filesystem::path& directoryPath )
+{
+	string str( directoryPath.string() );
+	str.erase( str.begin(), str.begin() + rootSize );
+
+	if ( str.find( "x64" ) != string::npos )
+		return;
+
+	if ( str.empty() )
+		str = "Root";
+
+	cout << string( depth, ' ' ) << "[+] " << str << endl;
+}
+
+void PrintFile( int depth, const filesystem::path& directoryPath )
+{
+	string str( directoryPath.string() );
+	str.erase( str.begin(), str.begin() + rootSize );
+	str.erase( str.begin(), str.begin() + 1 );
+
+	string::size_type h;
+	h = str.find( ".h" );
+
+	if ( str.find( "x64" ) != string::npos )
+		return;
+
+	if ( h != string::npos )
+	{
+		if ( str.find( "pch.h" ) == string::npos )
+		{
+			g_hVec.push_back( str );
+			cout << string( depth, ' ' ) << "[-] " << str << endl;
+		}
+	}
+
+	string::size_type cpp;
+	cpp = str.find( ".cpp" );
+
+	if ( cpp != string::npos )
+	{
+		if ( str.find( "pch.cpp" ) == string::npos )
+		{
+			g_cppVec.push_back( str );
+			cout << string( depth, ' ' ) << "[-] " << str << endl;
+		}
+	}
+}
+
+void TraverseDirectory( const filesystem::path& directoryPath, int depth = 0 )
+{
+	if ( !filesystem::exists( directoryPath ) )
+	{
+		cout << "Directory does not exist: " << directoryPath << endl;
+		return;
+	}
+
+	if ( filesystem::is_directory( directoryPath ) )
+	{
+		PrintDirectory( depth, directoryPath );
+
+		for ( const auto& entry : filesystem::directory_iterator( directoryPath ) )
+		{
+			TraverseDirectory( entry.path(), depth + 1 );
+		}
+	}
+	else if ( filesystem::is_regular_file( directoryPath ) )
+	{
+		PrintFile( depth, directoryPath );
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	TraverseDirectory( g_rootDirectory );
+
+	string   projectFilePath = "ProjectGenerator.vcxproj";
+	ifstream projectFile( projectFilePath );
+	string   projectContent( ( istreambuf_iterator<char>( projectFile ) ), istreambuf_iterator<char>() );
+
+
+	string   filtersFilePath = "ProjectGenerator.vcxproj.filters";
+	ifstream filtersFile( filtersFilePath );
+	string   filtersContent( ( istreambuf_iterator<char>( filtersFile ) ), istreambuf_iterator<char>() );
+
+
+	// 프로젝트 파일에 cpp 추가
+	for ( string& filePath : g_cppVec )
+	{
+		string itemElement = "<ClCompile Include=\"" + filePath + "\" />\n";
+
+		if ( projectContent.find( itemElement ) != string::npos )
+			continue;
+
+		projectContent.insert( projectContent.find( "<ClCompile Include=\"ProjectGenerator.cpp\" />" ), itemElement );
+	}
+
+	// 프로젝트 파일에 h 추가
+	for ( string& filePath : g_hVec )
+	{
+		string itemElement = "<ClInclude Include=\"" + filePath + "\" />\n";
+
+		if ( projectContent.find( itemElement ) != string::npos )
+			continue;
+
+		projectContent.insert( projectContent.find( "<ClInclude Include=\"pch.h\" />" ), itemElement );
+	}
+
+	string source = "Source\\";
+
+	// 필터 파일에 필터 추가
+	for ( string& filePath : g_cppVec )
+	{
+		if ( filePath.find( "\\" ) == string::npos )
+			continue;
+
+		string itemElement = "<Filter Include =\"";
+
+		string item = filePath;
+		reverse( item.begin(), item.end() );
+		item.erase( item.begin(), item.begin() + item.find( '\\' ) + 1 );
+		reverse( item.begin(), item.end() );
+
+		itemElement += ( source + item );
+
+		itemElement += "\"></Filter>\n";
+
+		if ( filtersContent.find( itemElement ) != string::npos )
+			continue;
+
+		string::size_type n = filtersContent.find( "<Filter Include=\"Source\">" );
+		filtersContent.insert( n, itemElement );
+	}
+
+	// 필터 파일에 cpp 추가
+	for ( string& filePath : g_cppVec )
+	{
+		if ( filePath.find( "\\" ) == string::npos )
+			continue;
+
+		string itemElement = "<ClCompile Include=\"" + filePath + "\">";
+		itemElement += "<Filter>";
+
+		string item = filePath;
+		reverse( item.begin(), item.end() );
+		item.erase( item.begin(), item.begin() + item.find( '\\' ) + 1 );
+		reverse( item.begin(), item.end() );
+
+		itemElement += ( source + item );
+
+		itemElement += "</Filter>";
+		itemElement += "</ClCompile>\n";
+
+		if ( filtersContent.find( itemElement ) != string::npos )
+			continue;
+
+		string::size_type n = filtersContent.find( "pch.cpp" );
+		n = filtersContent.find( "</ItemGroup>", n );
+
+		filtersContent.insert( n, itemElement );
+	}
+
+	// 필터 파일에 h 추가
+	for ( string& filePath : g_hVec )
+	{
+		if ( filePath.find( "\\" ) == string::npos )
+			continue;
+
+		string itemElement = "<ClInclude Include=\"" + filePath + "\">";
+		itemElement += "<Filter>";
+
+		string item = filePath;
+		reverse( item.begin(), item.end() );
+		item.erase( item.begin(), item.begin() + item.find( '\\' ) + 1 );
+		reverse( item.begin(), item.end() );
+
+		itemElement += ( source + item );
+
+		itemElement += "</Filter>";
+		itemElement += "</ClInclude>\n";
+
+		if ( filtersContent.find( itemElement ) != string::npos )
+			continue;
+
+		string::size_type n = filtersContent.find( "pch.h" );
+		n = filtersContent.find( "</ItemGroup>", n );
+
+		filtersContent.insert( n, itemElement );
+	}
+
+
+	ofstream updatedfiltersFile( filtersFilePath );
+	updatedfiltersFile << filtersContent;
+	updatedfiltersFile.close();
+
+	filtersFile.close();
+
+	ofstream updatedProjectFile( projectFilePath );
+	updatedProjectFile << projectContent;
+	updatedProjectFile.close();
+
+	projectFile.close();
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
